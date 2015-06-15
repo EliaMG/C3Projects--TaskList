8ad69b51a435e5b52122a7527439a20b0a446452
Carly Jugler <carjug@gmail.com>
2015-06-15 14:39:16 -0700

    update method in task.rb

--- lib/task.rb
+++ lib/task.rb
@@ -3,19 +3,20 @@ require 'sqlite3'
 module TaskList
   class Task < Database
 
-     # def validate_input(name)
-     #   raise ArgumentError.new "You must enter a task name" if name.to_s == ""
-     # end
-
-
     def all_tasks
-    statement = "select * from tasklist;"
-    query!(statement)
+      statement = "select * from tasklist;"
+      query!(statement)
     end
 
     def add_task(name, description, completed_date)
       # validate_input(name)
       query!("insert into tasklist (name, description, completed_date) VALUES (?, ?, ?);", name.to_s, description.to_s, completed_date.to_s)
+    end
+
+    def update_date(id)
+      @time = Time.now.strftime("%d/%m/%Y")
+      current_id = id
+      query!("update tasklist SET (completed_date=#{@time}) where (id = #{current_id});")
     end
 
   private
