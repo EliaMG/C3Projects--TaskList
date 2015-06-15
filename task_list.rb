305132aa976717ef008fb0e04535d02d05ecaa53
Elia <elgrenier@gmail.com>
2015-06-15 15:54:35 -0700

    something weird is happening

--- task_list.rb
+++ task_list.rb
@@ -24,6 +24,15 @@ class TaskSite < Sinatra::Base
     query = TaskList::Task.new("tasklist")
     query.add_task(params[:name], params[:description], params[:completed_date])
     @all_tasks = query.all_tasks
+    @new_array = []
+    @completed_tasks = []
+      @all_tasks.each do |task|
+        if task[3] == ""
+          @new_array.push(task)
+        else
+          @completed_tasks.push(task)
+        end
+      end
     @id = params[:task].to_i
     query.update_date(@id)
     erb :home
